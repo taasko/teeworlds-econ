b6c5cac2fd7966245ccbc9cc4e35c181ce63c882
Richard Lewis <richrd.lewis@gmail.com>
2020-05-09 13:39:51 +0300

    Packaging preparations.

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,1 @@
-import { TwEconClient } from "./TwEconClient";
-
-const host = "localhost";
-const port = 8765;
-const password = "";
-const econ = new TwEconClient(host, port, password);
-
-econ.on("game.start", (event) => {
-    console.log(event);
-});
-
-econ.connect();
+import { TwEconCLient } from "./TwEconCLient";
